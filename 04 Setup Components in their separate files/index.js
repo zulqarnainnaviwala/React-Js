56598bc5332870e472ec86d9007ed870fb768750
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2023-10-09 23:09:27 +0500

    React Component

---
+++ 04 Setup Components in their separate files/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MainContent from "./MainContent";
+
+function App() {
+  return (
+    <div>
+      <MainContent />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
