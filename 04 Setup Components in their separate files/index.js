856500159e8090ac4ad6a00c62f06bbfbc69df20
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2023-10-10 21:15:01 +0500

    header, footer added

--- 04 Setup Components in their separate files/index.js
+++ 04 Setup Components in their separate files/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 import MainContent from "./MainContent";
 
 function App() {
   return (
     <div>
+      <Header />
       <MainContent />
+      <Footer />
     </div>
   );
 }
