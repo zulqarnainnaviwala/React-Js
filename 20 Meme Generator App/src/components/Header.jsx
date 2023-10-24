123aab93116c6295e0879949f7cdd3b5dccdb353
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2023-10-24 18:51:08 +0500

    CSS , Added Async Call, Final Meme Generator App

---
+++ 20 Meme Generator App/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import troll from "../images/troll-face.png";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src={troll} className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
+  );
+}
